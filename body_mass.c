9c90cfdef0e8c6bcf895aa22a8e2bcf06741488c
nikolakoja93 <lifestopper93@gmail.com>
2016-08-31 16:28:38 +0200

    Uneo niz u program

--- body_mass.c
+++ body_mass.c
@@ -7,6 +7,7 @@
   float BMI = 0;
   float weight = 0;
   float height = 0;
+  int a[n];
 
   printf("unesi koliko ljudi\n");
   scanf("%d", &n);
@@ -16,13 +17,15 @@
   printf("unesi tezinu tela i visinu tela\n");
   scanf("%f %f", &weight, &height);
 }
+  for (i=0;i<n;i++) {
   BMI = weight / ((height)*(height));
-   if (BMI < 18.5) {
-  printf("under\n"); }
-   if (BMI >= 18.5 || BMI < 25.0) {
-  printf("normal\n"); }
-   if (BMI >= 25.0 || BMI < 30.0) {
-  printf("over\n"); }
-   if (BMI >= 30.0) {
-  printf("obese\n"); }
-}
+  a[n] = BMI;
+   if (a[n] < 18.5) {
+  printf("under  "); }
+   else if (a[n] >= 18.5 || a[n] < 25.0) {
+  printf("normal  "); }
+   else if (a[n] >= 25.0 || a[n] < 30.0) {
+  printf("over  "); }
+   else if (a[n] >= 30.0) {
+  printf("obese  "); }
+}}
