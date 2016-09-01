3a9fbac0d573721aade281401a9f8e2846021935
nikolakoja93 <lifestopper93@gmail.com>
2016-09-01 18:50:08 +0200

    Rad u novom meniju

--- body_mass.c
+++ body_mass.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 
- meni (int choice, int i, double* user_weight, double* user_height,double* BMI) {
-  printf("-------Menu-------\n");
-  printf("1) Ponovni unos\n");
-  printf("2) Exit\n");
-  scanf("%d", &choice);
-  if (choice==1)
-     {
-        printf("Izabrali ste ponovni unos podataka\n");
-        prvi_prvi ( i, user_weight, user_height, BMI);
-        meni(choice, i, user_weight, user_height, BMI);
-     }
-     else if (choice==2)
-     {
-        printf("Exit!\n");
-        return 0;
-     }
+ drugi_drugi (int i, int n, double* user_weight, double* user_height, double* BMI) {
+  printf("unesi koliko ljudi\n");
+  scanf("%d", &n);
+
+  for (i=0;i<n;i++) {
+    printf("unesi tezinu tela i visinu tela\n");
+    scanf("%lf %lf", &user_weight[i], &user_height[i]);
 }
+  for (i=0;i<n;i++) {
+   BMI[i] = user_weight[i] / ((user_height[i])*(user_height[i]));
+     if (BMI[i] < 18.5)
+    printf("under  ");
+     else if (BMI[i] > 18.5 && BMI[i] <= 25.0)
+    printf("normal  ");
+     else if (BMI[i] > 25.0 && BMI[i] <= 30.0)
+    printf("over  ");
+     else if (BMI[i] > 30.0)
+    printf("obese  \n");
+}}
 
   prvi_prvi (int i, double* user_weight, double* user_height, double* BMI) {
   printf("unesi tezinu tela i visinu tela\n");
@@ -39,15 +41,39 @@
   printf("%lf\n", BMI[i]); }
 }
 
- int main() {
-  int choice;
+ int main(void) {
+  int choice=0;
   int i;
   int n;
   double BMI[100];
   double user_weight[100];
   double user_height[100];
 
-  prvi_prvi( i, user_weight, user_height, BMI);
-
-  meni(choice, i, user_weight, user_height, BMI);
+  while(choice!='4')
+ {
+   printf("\n\t 1. BMI za jednu osobu");
+   printf("\n\t 2. BMI za vise osoba");
+   printf("\n\t 3. Exit");
+   printf("\n\n Izaberite sta zelite: ");
+   choice = getchar();
+   switch(choice)
+   {
+   case '1':
+     printf("\n\nBMI za jednu osobu\n");
+     prvi_prvi ( i, user_weight, user_height, BMI ); 
+     break;
+   case '2':
+     printf("\n\nBMI za vise osoba\n");
+     drugi_drugi ( i, n, user_weight, user_height, BMI);
+     break;
+   case '3':
+     printf("\n\nExit\n");
+     return 0;
+   default:
+     printf("\n\nINVALID SELECTION...Please try again\n");
+   }
+   (void)getchar();
+ }
+ return 0;
 }
+
